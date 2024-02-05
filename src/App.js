e7a02c307b390fb692bb978b1022936854ae621e
Alex B <137462408+Albee89@users.noreply.github.com>
2024-02-05 13:41:06 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,41 +9,30 @@ import About from './Components/About/About';
 import SportsClubs from './Components/SportsClubs/SportsClubs';
 import WeatherInformation from './Components/Main/WeatherInfomation/WeatherInformation';
 
-
-
-
-function App () {
+function App() {
   return (
     <div className="App">
- <Navigation />
-    <Header />
-  
-    
-  
-    <div className="main">
-    <Main />
-  
-  
-    </div>  
-    <TikTok />
-    <SportsClubs/>
-    <WeatherInformation />
-    
-  
- <div>
-  <About />
- 
-  
-  </div>
-  <div>
-  <div><br>
-    
-  </br></div>
-  <Footer />
+      <Navigation />
+      <Header />
+
+      <div className="main">
+        <Main />
+      </div>
+
+      <TikTok />
+      <SportsClubs />
+      <WeatherInformation />
+
+      <div>
+        <About />
+      </div>
+
+      <div>
+        <div><br></br></div>
+        <Footer />
+      </div>
     </div>
-    </div>
-  
-  )
-};
+  );
+}
 
 export default App;
